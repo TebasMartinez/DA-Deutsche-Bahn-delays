e71571274fd89cdbb967a055becf5eff527168e1
Tebas Martínez <203688711+TebasMartinez@users.noreply.github.com>
2025-08-05 16:02:13 +0200

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+def filter(df):
+    df = df[df["train_type"].isin(["ICE", "IC", "RE", "RB", "S", "IRE"])]
+    
+    return df
+
+def clean(df):
+    df = df.drop(columns=["arrival_planned_time", "arrival_change_time", "departure_planned_time", "departure_change_time"])
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+def group_west_east(df):
+    west_germany_stations = [
+    'Bremen Hbf', 'Lübeck Hbf', 'Heilbronn Hbf', 'Regensburg Hbf',
+    'Aachen Hbf', 'Aschaffenburg Hbf', 'Augsburg Hbf', 'Bad Oldesloe',
+    'Bamberg', 'Bielefeld Hbf', 'Bietigheim-Bissingen', 'Bochum Hbf',
+    'Bonn Hbf', 'Braunschweig Hbf', 'Bruchsal', 'Darmstadt Hbf',
+    'Singen(Hohentwiel)', 'Dortmund Hbf', 'Düsseldorf Flughafen',
+    'Düsseldorf Hbf', 'Duisburg Hbf', 'Solingen Hbf', 'Stuttgart Hbf',
+    'Essen Hbf', 'Frankfurt(Main)Hbf', 'Freiburg(Breisgau) Hbf',
+    'Fürth(Bay)Hbf', 'Fulda', 'Gelsenkirchen Hbf', 'Gießen',
+    'Göttingen', 'Trier Hbf', 'Tübingen Hbf', 'Hagen Hbf',
+    'Hamburg-Harburg', 'Hamm(Westf)Hbf', 'Hanau Hbf', 'Hannover Hbf',
+    'Heidelberg Hbf', 'Herford', 'Uelzen', 'Hildesheim Hbf', 'Ulm Hbf',
+    'Ingolstadt Hbf', 'Kaiserslautern Hbf', 'Karlsruhe Hbf',
+    'Kassel Hbf', 'Kiel Hbf', 'Koblenz Hbf', 'Köln Hbf',
+    'Landshut(Bay)Hbf', 'Ludwigshafen(Rh)Hbf', 'Lüneburg', 'Mainz Hbf',
+    'Mannheim Hbf', 'Wiesbaden Hbf', 'Mönchengladbach Hbf',
+    'Worms Hbf', 'Würzburg Hbf', 'München Hbf', 'München Ost',
+    'Münster(Westf)Hbf', 'Wuppertal Hbf', 'Neumünster', 'Neuss Hbf',
+    'Neustadt(Weinstr)Hbf', 'Nürnberg Hbf', 'Oberhausen Hbf',
+    'Offenburg', 'Oldenburg(Oldb)Hbf', 'Osnabrück Hbf',
+    'Paderborn Hbf', 'Pforzheim Hbf', 'Plochingen', 'Rheine',
+    'Rosenheim', 'Saarbrücken Hbf', 'Düsseldorf Flughafen Terminal',
+    'Frankfurt(Main)Süd', 'Hamburg Dammtor', 'Hamburg Hbf',
+    'Hamburg-Altona', 'Kassel-Wilhelmshöhe', 'Köln Messe/Deutz',
+    'München-Pasing', 'Wolfsburg Hbf'
+    ]
+    
+    east_germany_stations = [
+    'Berlin-Lichtenberg', 'Cottbus Hbf', 'Dresden Hbf', 'Dresden-Neustadt',
+    'Erfurt Hbf', 'Frankfurt(Oder)', 'Halle(Saale)Hbf', 'Chemnitz Hbf',
+    'Leipzig Hbf', 'Magdeburg Hbf', 'Berlin Ostbahnhof', 'Rostock Hbf',
+    'Berlin-Spandau', 'Berlin-Wannsee', 'Berlin Zoologischer Garten',
+    'Berlin Gesundbrunnen', 'Berlin Südkreuz', 'Berlin Potsdamer Platz',
+    'Berlin Hbf', 'Flughafen BER', 'Berlin Friedrichstraße',
+    'Potsdam Hbf', 'Weimar', 'Berlin Ostkreuz'
+    ]
+
+    df.loc[df["station"].isin(west_germany_stations), "former"] = "West"
+    df.loc[df["station"].isin(east_germany_stations), "former"] = "East"
+    
+    return df
+            
