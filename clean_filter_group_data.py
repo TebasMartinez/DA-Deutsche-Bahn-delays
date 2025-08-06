4cdc7dd34551cfa10cd747060f3f78a43c76c254
Tebas Martínez <203688711+TebasMartinez@users.noreply.github.com>
2025-08-06 17:04:13 +0200

    Create clean_filter_group_data.py

---
+++ clean_filter_group_data.py
@@ -0,0 +1,18 @@
+"""
+Uses functions from functions.py to clean, filter, and group data from combineddata.csv, and outputs clean_grouped_combineddata.csv, storing it in the data directory. The new file can be used to create Tableau dashboards.
+"""
+
+import functions
+import pandas as pd
+
+def main():
+    df = pd.read_csv("data/combineddata.csv")
+    df = functions.filter(df)
+    df = functions.clean(df)
+    df = functions.group_west_east(df)
+    df = functions.group_dates_seasons(df)
+    df = functions.group_puctuality(df)
+    df.to_csv("data/clean_grouped_combineddata.csv")
+
+main()
+    
